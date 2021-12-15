1efd383e8fafae8cc884f99c6c37a4704f684607
pnkj-antino <pankaj@antino.io>
2021-12-15 18:36:16 +0530

    Firebase analytics and crashylytics integration

--- fitbasix/lib/feature/log_in/view/login_screen.dart
+++ fitbasix/lib/feature/log_in/view/login_screen.dart
@@ -1,6 +1,9 @@
-import 'package:fitbasix/feature/log_in/controller/login_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'package:fitbasix/core/analytics/analytics_service.dart';
+import 'package:fitbasix/feature/log_in/controller/login_controller.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -9,13 +12,24 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return Scaffold(
-      body: Center(
-          child: TextButton(
-              onPressed: () {
-                _loginController.googleLogin();
-              },
-              child: const Text('Google Signup'))),
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () async {
+                  _loginController.googleLogin();
+                  await AnalyticsService.analytics
+                      .logSignUp(signUpMethod: 'google_signup');
+                },
+                child: const Text('Google Signup')),
+            Text(user!.displayName! + ' is Logged In')
+          ],
+        )),
+      ),
     );
   }
 }
--- fitbasix/lib/fitbasix_app.dart
+++ fitbasix/lib/fitbasix_app.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:fitbasix/core/analytics/analytics_service.dart';
 import 'package:fitbasix/core/routes/app_routes.dart';
 import 'package:fitbasix/feature/log_in/view/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:get/get.dart';
 
 class FitBasixApp extends StatelessWidget {
   const FitBasixApp({Key? key}) : super(key: key);
@@ -18,6 +20,7 @@ class FitBasixApp extends StatelessWidget {
         supportedLocales: context.supportedLocales,
         locale: context.locale,
         onGenerateRoute: GenerateRoute.generateRoute,
+        navigatorObservers: [AnalyticsService.getAnalyticsObserver()],
         home: LoginScreen());
   }
 }
