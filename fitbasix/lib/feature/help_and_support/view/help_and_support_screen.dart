1055a44511bf1f2775b767bb6eaff051a101e61f
himankantino <99168358+himankantino@users.noreply.github.com>
2022-02-22 19:07:25 +0530

    update willpop scope

--- fitbasix/lib/feature/help_and_support/view/help_and_support_screen.dart
+++ fitbasix/lib/feature/help_and_support/view/help_and_support_screen.dart
@@ -29,139 +29,146 @@ class HelpAndSupportScreen extends StatelessWidget {
       _helpAndSupportController.getAllHelpAndSupportContent();
     }
 
-    return Scaffold(
-        backgroundColor: kPureWhite,
-        appBar: AppBarForAccount(
-          title: "help_support".tr,
-          onback: () {
-            _homeController.selectedIndex.value = 0;
-            Navigator.pop(context);
-            Navigator.pop(context);
-          },
-        ),
-        body: Obx(
-          () => _helpAndSupportController.isLoading.value
-              ? Center(child: CustomizedCircularProgress())
-              : ListView(
-                  physics: const BouncingScrollPhysics(),
-                  children: [
-                    //whatsapp and call buttons
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 16 * SizeConfig.widthMultiplier!,
-                          vertical: 16 * SizeConfig.heightMultiplier!),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            _helpAndSupportController.helpAndSupportDataModel!
-                                .response!.data!.description!,
-                            style: AppTextStyle.black400Text,
-                          ),
-                          SizedBox(
-                            height: 16 * SizeConfig.heightMultiplier!,
-                          ),
-                          Row(
-                            children: [
-                              //whatsapp button
-                              Expanded(
-                                child: InkWell(
-                                  onTap: () {
-                                    launch(
-                                        "https://Wa.me/${_helpAndSupportController.helpAndSupportDataModel!.response!.data!.whatsAppNo}");
-                                  },
-                                  child: Container(
-                                    height: 48 * SizeConfig.heightMultiplier!,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(
-                                            8 * SizeConfig.widthMultiplier!),
-                                        color: kGreenColor),
-                                    child: Center(
-                                      child: SvgPicture.asset(
-                                        ImagePath.whatsAppIcon,
-                                        height: 24 *
-                                            SizeConfig.imageSizeMultiplier!,
+    return WillPopScope(
+      onWillPop: () async{
+        Navigator.pop(context);
+        Navigator.pop(context);
+        return false;
+      },
+      child: Scaffold(
+          backgroundColor: kPureWhite,
+          appBar: AppBarForAccount(
+            title: "help_support".tr,
+            onback: () {
+              _homeController.selectedIndex.value = 0;
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
+          ),
+          body: Obx(
+            () => _helpAndSupportController.isLoading.value
+                ? Center(child: CustomizedCircularProgress())
+                : ListView(
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      //whatsapp and call buttons
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 16 * SizeConfig.widthMultiplier!,
+                            vertical: 16 * SizeConfig.heightMultiplier!),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              _helpAndSupportController.helpAndSupportDataModel!
+                                  .response!.data!.description!,
+                              style: AppTextStyle.black400Text,
+                            ),
+                            SizedBox(
+                              height: 16 * SizeConfig.heightMultiplier!,
+                            ),
+                            Row(
+                              children: [
+                                //whatsapp button
+                                Expanded(
+                                  child: InkWell(
+                                    onTap: () {
+                                      launch(
+                                          "https://Wa.me/${_helpAndSupportController.helpAndSupportDataModel!.response!.data!.whatsAppNo}");
+                                    },
+                                    child: Container(
+                                      height: 48 * SizeConfig.heightMultiplier!,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                              8 * SizeConfig.widthMultiplier!),
+                                          color: kGreenColor),
+                                      child: Center(
+                                        child: SvgPicture.asset(
+                                          ImagePath.whatsAppIcon,
+                                          height: 24 *
+                                              SizeConfig.imageSizeMultiplier!,
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 16 * SizeConfig.widthMultiplier!,
-                              ),
-                              //call button
-                              Expanded(
-                                child: InkWell(
-                                  onTap: () {
-                                    ///launch call with help no
-                                    launch(
-                                        "tel://${_helpAndSupportController.helpAndSupportDataModel!.response!.data!.callingNo}");
-                                  },
-                                  child: Container(
-                                    height: 48 * SizeConfig.heightMultiplier!,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(
-                                            8 * SizeConfig.widthMultiplier!),
-                                        color: kGreenColor),
-                                    child: Center(
-                                      child: SvgPicture.asset(
-                                        ImagePath.phoneCallIcon,
-                                        height: 26 *
-                                            SizeConfig.imageSizeMultiplier!,
-                                        width: 26 *
-                                            SizeConfig.imageSizeMultiplier!,
+                                SizedBox(
+                                  width: 16 * SizeConfig.widthMultiplier!,
+                                ),
+                                //call button
+                                Expanded(
+                                  child: InkWell(
+                                    onTap: () {
+                                      ///launch call with help no
+                                      launch(
+                                          "tel://${_helpAndSupportController.helpAndSupportDataModel!.response!.data!.callingNo}");
+                                    },
+                                    child: Container(
+                                      height: 48 * SizeConfig.heightMultiplier!,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                              8 * SizeConfig.widthMultiplier!),
+                                          color: kGreenColor),
+                                      child: Center(
+                                        child: SvgPicture.asset(
+                                          ImagePath.phoneCallIcon,
+                                          height: 26 *
+                                              SizeConfig.imageSizeMultiplier!,
+                                          width: 26 *
+                                              SizeConfig.imageSizeMultiplier!,
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 16 * SizeConfig.heightMultiplier!,
-                      color: kLightGrey,
-                    ),
-                    SizedBox(
-                      height: 24 * SizeConfig.heightMultiplier!,
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: 16 * SizeConfig.widthMultiplier!,
-                            bottom: 16 * SizeConfig.heightMultiplier!),
-                        child: Text(
-                          "FAQs".tr,
-                          style: AppTextStyle.boldBlackText,
-                        )),
-                    Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: List.generate(
-                            _helpAndSupportController
-                                .helpAndSupportDataModel!
-                                .response!
-                                .data!
-                                .questionsAndAnswers!
-                                .length, (FAQIndex) {
-                          return ExpandableWidget(
-                            title: _helpAndSupportController
-                                .helpAndSupportDataModel!
-                                .response!
-                                .data!
-                                .questionsAndAnswers![FAQIndex]
-                                .question,
-                            content: _helpAndSupportController
-                                .helpAndSupportDataModel!
-                                .response!
-                                .data!
-                                .questionsAndAnswers![FAQIndex]
-                                .answer,
-                          );
-                        }))
-                  ],
-                ),
-        ));
+                      Container(
+                        width: double.infinity,
+                        height: 16 * SizeConfig.heightMultiplier!,
+                        color: kLightGrey,
+                      ),
+                      SizedBox(
+                        height: 24 * SizeConfig.heightMultiplier!,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 16 * SizeConfig.widthMultiplier!,
+                              bottom: 16 * SizeConfig.heightMultiplier!),
+                          child: Text(
+                            "FAQs".tr,
+                            style: AppTextStyle.boldBlackText,
+                          )),
+                      Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: List.generate(
+                              _helpAndSupportController
+                                  .helpAndSupportDataModel!
+                                  .response!
+                                  .data!
+                                  .questionsAndAnswers!
+                                  .length, (FAQIndex) {
+                            return ExpandableWidget(
+                              title: _helpAndSupportController
+                                  .helpAndSupportDataModel!
+                                  .response!
+                                  .data!
+                                  .questionsAndAnswers![FAQIndex]
+                                  .question,
+                              content: _helpAndSupportController
+                                  .helpAndSupportDataModel!
+                                  .response!
+                                  .data!
+                                  .questionsAndAnswers![FAQIndex]
+                                  .answer,
+                            );
+                          }))
+                    ],
+                  ),
+          )),
+    );
   }
 }
--- fitbasix/lib/feature/help_and_support/view/privacy_policy_and_term_of_use/legal_screen.dart
+++ fitbasix/lib/feature/help_and_support/view/privacy_policy_and_term_of_use/legal_screen.dart
@@ -16,79 +16,86 @@ class LegalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final HomeController _homeController = Get.find();
-    return Scaffold(
-      backgroundColor: kPureWhite,
-      appBar: AppBarForAccount(
-        title: "legal".tr,
-        onback: () {
-          _homeController.selectedIndex.value = 0;
-          Navigator.pop(context);
-          Navigator.pop(context);
-        },
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 30 * SizeConfig.heightMultiplier!,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: 18 * SizeConfig.widthMultiplier!),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, RouteName.privacyAndPolicy);
-              },
-              child: Row(
-                children: [
-                  GestureDetector(
-                      child: SvgPicture.asset(
-                    ImagePath.privacyPolicyIcon,
-                    width: 16 * SizeConfig.widthMultiplier!,
-                    height: 20 * SizeConfig.heightMultiplier!,
-                    fit: BoxFit.contain,
-                  )),
-                  SizedBox(
-                    width: 16.59 * SizeConfig.widthMultiplier!,
-                  ),
-                  Text(
-                    "privacy_policy".tr,
-                    style: AppTextStyle.NormalBlackTitleText,
-                  ),
-                ],
+    return WillPopScope(
+      onWillPop: () async{
+        Navigator.pop(context);
+        Navigator.pop(context);
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: kPureWhite,
+        appBar: AppBarForAccount(
+          title: "legal".tr,
+          onback: () {
+            _homeController.selectedIndex.value = 0;
+            Navigator.pop(context);
+            Navigator.pop(context);
+          },
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 30 * SizeConfig.heightMultiplier!,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 18 * SizeConfig.widthMultiplier!),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, RouteName.privacyAndPolicy);
+                },
+                child: Row(
+                  children: [
+                    GestureDetector(
+                        child: SvgPicture.asset(
+                      ImagePath.privacyPolicyIcon,
+                      width: 16 * SizeConfig.widthMultiplier!,
+                      height: 20 * SizeConfig.heightMultiplier!,
+                      fit: BoxFit.contain,
+                    )),
+                    SizedBox(
+                      width: 16.59 * SizeConfig.widthMultiplier!,
+                    ),
+                    Text(
+                      "privacy_policy".tr,
+                      style: AppTextStyle.NormalBlackTitleText,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 30 * SizeConfig.heightMultiplier!,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: 16 * SizeConfig.widthMultiplier!),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, RouteName.termOfUse);
-              },
-              child: Row(
-                children: [
-                  GestureDetector(
-                      child: SvgPicture.asset(
-                    ImagePath.termOfUseIcon,
-                    width: 20 * SizeConfig.imageSizeMultiplier!,
-                    height: 20 * SizeConfig.imageSizeMultiplier!,
-                    fit: BoxFit.contain,
-                  )),
-                  SizedBox(
-                    width: 15 * SizeConfig.widthMultiplier!,
-                  ),
-                  Text(
-                    "term_of_use".tr,
-                    style: AppTextStyle.NormalBlackTitleText,
-                  ),
-                ],
+            SizedBox(
+              height: 30 * SizeConfig.heightMultiplier!,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 16 * SizeConfig.widthMultiplier!),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, RouteName.termOfUse);
+                },
+                child: Row(
+                  children: [
+                    GestureDetector(
+                        child: SvgPicture.asset(
+                      ImagePath.termOfUseIcon,
+                      width: 20 * SizeConfig.imageSizeMultiplier!,
+                      height: 20 * SizeConfig.imageSizeMultiplier!,
+                      fit: BoxFit.contain,
+                    )),
+                    SizedBox(
+                      width: 15 * SizeConfig.widthMultiplier!,
+                    ),
+                    Text(
+                      "term_of_use".tr,
+                      style: AppTextStyle.NormalBlackTitleText,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
