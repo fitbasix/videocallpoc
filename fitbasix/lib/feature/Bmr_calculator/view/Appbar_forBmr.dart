43c4eb26ba89e523b745f4f909e1e8b39345ef66
vartikamangal <88431640+vartikaantino@users.noreply.github.com>
2022-02-22 16:01:20 +0530

    increased gesturable area

--- fitbasix/lib/feature/Bmr_calculator/view/Appbar_forBmr.dart
+++ fitbasix/lib/feature/Bmr_calculator/view/Appbar_forBmr.dart
@@ -18,36 +18,20 @@ class AppbarforBMRScreen extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      automaticallyImplyLeading: false,
       backgroundColor: kPureWhite,
       elevation: 0,
-      title: Row(
-        children: [
-          // SizedBox(
-          //   width: 7 * SizeConfig.widthMultiplier!,
-          // ),
-          GestureDetector(
-              onTap: onRoute,
-              child: Container(
-                color: Colors.transparent,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SvgPicture.asset(
-                    ImagePath.backIcon,
-                    width: 7.41 * SizeConfig.widthMultiplier!,
-                    height: 12 * SizeConfig.heightMultiplier!,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              )),
-          SizedBox(
-            width: 20.59 * SizeConfig.widthMultiplier!,
-          ),
-          Text(
-            title!,
-            style: AppTextStyle.NormalBlackTitleText,
-          ),
-        ],
+      leading: IconButton(
+        onPressed: onRoute,
+        icon: SvgPicture.asset(
+          ImagePath.backIcon,
+          width: 7.41 * SizeConfig.widthMultiplier!,
+          height: 12 * SizeConfig.heightMultiplier!,
+          fit: BoxFit.contain,
+        ),
+      ),
+      title: Text(
+        title!,
+        style: AppTextStyle.NormalBlackTitleText,
       ),
       actions: [
         IconButton(
