43bc526550b5425a66164a9a50b8326aa9533551
KashifAhmadAntino <99168342+KashifAhmadAntino@users.noreply.github.com>
2022-03-03 19:43:10 +0530

    added water intake notification

--- fitbasix/lib/main_dev.dart
+++ fitbasix/lib/main_dev.dart
@@ -1,19 +1,53 @@
+import 'dart:isolate';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:fitbasix/setup-my-app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+
+initializeNotification(){
+  AwesomeNotifications().initialize(
+    // set the icon to null if you want to use the default app icon
+      'resource://drawable/background',
+      [
+        NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            defaultColor: Color(0xFF9D50DD),
+            ledColor: Colors.white)
+      ],
+      // Channel groups are only visual and are not required
+      channelGroups: [
+        NotificationChannelGroup(
+            channelGroupkey: 'basic_channel_group',
+            channelGroupName: 'Basic group')
+      ],
+      debug: true
+  );
+}
+
 
 Future<void> main() async {
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    initializeNotification();
     await Firebase.initializeApp();
+    await AndroidAlarmManager.initialize();
     final SharedPreferences prefs = await SharedPreferences.getInstance();
+
     var accessToken = prefs.getString('AccessToken');
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
     setupApp();
+    //await AndroidAlarmManager.oneShotAt(date, 0, printHello);
   }, (error, stackTrace) {
     FirebaseCrashlytics.instance.recordError(error, stackTrace);
   });
 }
+
+
